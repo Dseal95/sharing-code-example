f14240d8435b33352c15ab16fa23401c66033f72
danielseal <dan.seal.06@gmail.com>
2022-04-25 15:16:18 +0100

    Updating repo structure 101

---
+++ src/common/utils.py
@@ -0,0 +1,8 @@
+"""utils.py module."""
+
+from datetime import datetime
+
+
+def useful_function():
+    """"""
+    print(f"The time is {datetime.now()}.")
---
+++ src/project1/main.py
@@ -0,0 +1,4 @@
+from common.utils import useful_function
+
+if '__main__' == __name__:
+    useful_function() 
